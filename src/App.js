35fc956365088618b1aa197c1f1eeb24ee31e018
Alejandro Estrada <aestrada611@gmail.com>
2021-01-05 13:51:06 -0800

    adds chuck norris api functionality

--- src/App.js
+++ src/App.js
@@ -4,10 +4,36 @@ import './App.css'
 // https://api.chucknorris.io/jokes/random
 
 class App extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			joke: null
+		}
+	}
+
+
+	getJoke = () => {
+	      fetch('https://api.chucknorris.io/jokes/random')
+	        .then((response) => {
+	          return response.json()
+	        })
+	        .then((payload) => {
+	          this.setState({ joke: payload.value })
+	        })
+	        .catch((error) => {
+	          console.log(error)
+	        })
+	    }
+
+
 	render() {
 		return (
 			<div className='App'>
 				<h1>Chuck Norris Jokes</h1>
+				<h4>See Random Chuck Norris Jokes</h4>
+				<button onClick={ this.getJoke }>Test Button</button>
+				<br/>
+				{this.state.joke}
 			</div>
 		)
 	}
