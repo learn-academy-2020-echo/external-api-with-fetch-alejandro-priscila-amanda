b48e4b07a11324533f48822cc11d13ff0023e4bc
priscila2294 <priscilla_2294@hotmail.com>
2021-01-05 14:55:55 -0800

    adds pictures

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import './App.css'
 
 // https://api.chucknorris.io/jokes/random
@@ -7,7 +7,10 @@ class App extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			joke: null
+			joke: null,
+			unsplash: null,
+			img: null
+
 		}
 	}
 
@@ -23,11 +26,29 @@ class App extends Component {
 	        .catch((error) => {
 	          console.log(error)
 	        })
-	    }
+		}
+		
+		getPic = () => {
+			fetch(
+			  'https://api.unsplash.com/photos/random/?client_id=T3G5x5RAcJCHMFD3sToyajQ0ZbklkrWsVvTMZM_incY'
+			)
+			  .then((response) => {
+				  console.log(this.state.unsplash)
+				return response.json()
+			  })
+			  .then((payload) => {
+				// console.log(payload[0].urls.small)
+				this.setState({ img: payload.urls.small })
+			  })
+			  .catch((error) => {
+				console.log(error)
+			  })
+		  }
 
-
+		
 	render() {
 		return (
+			<Fragment>
 			<div className='App'>
 				<h1>Chuck Norris Jokes</h1>
 				<h4>See Random Chuck Norris Jokes</h4>
@@ -35,6 +56,21 @@ class App extends Component {
 				<br/>
 				{this.state.joke}
 			</div>
+
+			<div className='App'>
+               <h1>Unsplash Photos</h1>
+               <button onClick={this.getPic}>Click Me!</button>
+               <br />
+               {this.state.img && (
+               <img
+              src={this.state.img}
+              alt='Random Imagine'
+            />
+            )}
+           </div>
+			</Fragment>
+
+
 		)
 	}
 }
